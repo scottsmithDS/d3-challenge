258f2f29befb978651c9abeb65cb135641eb6a02
scottsmithDS <shsmithsd@gmail.com>
2020-05-30 14:48:02 -0700

    commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// @TODO: YOUR CODE HERE!
+// Load data from hours-of-tv-watched.csv
+d3.csv("../data/data.csv").then(function(data) {
+
+    console.log(data);
+  
+    // log a list of names
+    var healthcare = data.map(data => data.healthcare);
+    var poverty = data.map(data => data.poverty);
+    var smokers = data.map(data => data.smokers);
+    var age = data.map(data => data.age);
+    console.log("healthcare", healthcare);
+
+});
+  
+    // Cast each hours value in tvData as a number using the unary + operator
+  //  tvData.forEach(function(data) {
+    //  data.hours = +data.hours;
+   //   console.log("Name:", data.name);
+   //   console.log("Hours:", data.hours);
+  //  });
+//  }).catch(function(error) {
+  //  console.log(error);
+ // });
+  
